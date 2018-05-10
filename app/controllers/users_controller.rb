f6208b91b97379cc82195dfdf839cb04436872df
Ababii Alex (Maki) <work.maki.5@gmail.com>
2018-05-10 10:09:48 +0300

    moved user after update logic to model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,21 +19,11 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update_attributes(user_params)
-
-    ActionCable.server.broadcast 'users_channel', user: render_user(@user), user_edit: render_user_edit(@user), id: @user.id.to_s
   end
 
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name)
-  end
-
-  def render_user(user)
-    ApplicationController.render(partial: 'users/user', locals: { user: user })
-  end
-
-  def render_user_edit(user)
-    ApplicationController.render(partial: 'users/user_edit', locals: { user: user })
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User
 
   field :first_name, type: String
   field :last_name, type: String
+
+  after_update :cable_update
+
+  def cable_update
+    ActionCable.server.broadcast 'users_channel', user: render_user(self), user_edit: render_user_edit(self), id: self.id.to_s
+  end
+
+  def render_user(user)
+    ApplicationController.render(partial: 'users/user', locals: { user: user })
+  end
+
+  def render_user_edit(user)
+    ApplicationController.render(partial: 'users/user_edit', locals: { user: user })
+  end
 end
